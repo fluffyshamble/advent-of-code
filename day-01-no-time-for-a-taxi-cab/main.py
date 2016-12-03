e9cf2ee427c2b8afbc5fb3553260e5bc4f3f96df
fluffyshamble <jad.gamblin@gmail.com>
2016-12-03 13:09:17 +0000

    added 'move' function

--- day-01-no-time-for-a-taxi-cab/main.py
+++ day-01-no-time-for-a-taxi-cab/main.py
@@ -4,18 +4,28 @@ def parse_direction(filename):
     return output
 
 
-def move(direction, facing):
+def move(direction):
+    facing = 0
+    pos = [0,0]
     if "L" in direction:
         facing -= 1
         facing = facing % 4
     elif "R" in direction:
         facing += 1
         facing = facing % 4
-
-
+    if facing == 0:
+        pos[1] +=1
+    elif facing == 1:
+        pos[0] += 1
+    elif facing == 2:
+        pos[1] -= 1
+    elif facing == 3:
+        pos[0] -= 1
+    return pos
 
 
 
 input = "directions.txt"
 directions = parse_direction(input)
+
 
