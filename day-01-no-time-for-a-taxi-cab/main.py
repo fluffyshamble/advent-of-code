fc51599effeb99c1071b9817bd8b572fc7db26f9
fluffyshamble <jad.gamblin@gmail.com>
2016-12-03 12:50:27 +0000

    added 'facing' function to define direction'

--- day-01-no-time-for-a-taxi-cab/main.py
+++ day-01-no-time-for-a-taxi-cab/main.py
@@ -1,8 +1,21 @@
-def parsedirection(filename):
+def parse_direction(filename):
     text_file = open(filename, "r")
     output = text_file.read().split(',')
     return output
 
+
+def move(direction, facing):
+    if "L" in direction:
+        facing -= 1
+        facing = facing % 4
+    elif "R" in direction:
+        facing += 1
+        facing = facing % 4
+
+
+
+
+
 input = "directions.txt"
-directions = parsedirection(input)
-print directions
+directions = parse_direction(input)
+
