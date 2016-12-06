933b957555c74d4b77e0ed2fac82b15ecad3ec97
fluffyshamble <jad.gamblin@gmail.com>
2016-12-06 13:54:12 +0000

    Part 2 Complete

--- day-06-signals-and-noise/main.py
+++ day-06-signals-and-noise/main.py
@@ -1,6 +1,6 @@
 import collections as c
 
-# read in signals
+
 def solve_part_one(filename):
     text_file = open(filename, "r")
     output = text_file.read().split('\n')
@@ -13,5 +13,18 @@ def solve_part_one(filename):
     return
 
 
+def solve_part_two(filename):
+    text_file = open(filename, "r")
+    output = text_file.read().split('\n')
+    message = ""
+    text_file.close()
+    signals = zip(*output)
+    for signal in signals:
+        message = message + c.Counter(signal).most_common()[-1][0]
+    print "Message Two: %s" % message
+    return
+
+
 if __name__ == '__main__':
     solve_part_one("signal.txt")
+    solve_part_two("signal.txt")
