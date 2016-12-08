47b54eea6b266a332329886cc64fda9ad5de0678
fluffyshamble <jad.gamblin@gmail.com>
2016-12-08 20:25:37 +0000

    added rect() function

--- day-08-two-factor-authentification/main.py
+++ day-08-two-factor-authentification/main.py
@@ -7,9 +7,16 @@ def create_screen(width, height):
         screen.append(new_line)
     return screen
 
+
 def print_screen(screen):
     print('\n'.join([''.join(['{:4}'.format(item) for item in row])
                      for row in screen]))
+
+
+def rect(width, height, screen):
+    for j in range(0, height):
+        for i in range(0, width):
+            screen[j][i] = '#'
 
 
 
@@ -17,6 +24,7 @@ screen_width = 6
 screen_height = 2
 
 screen = create_screen(screen_width, screen_height)
+rect(3,2,screen)
 print_screen(screen)
 
 
