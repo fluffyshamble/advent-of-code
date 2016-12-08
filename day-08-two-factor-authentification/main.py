8807b0fce69bc23e99fcbf16ca261f79c249afca
fluffyshamble <jad.gamblin@gmail.com>
2016-12-08 20:16:09 +0000

    added print_screen()

--- day-08-two-factor-authentification/main.py
+++ day-08-two-factor-authentification/main.py
@@ -7,13 +7,18 @@ def create_screen(width, height):
         screen.append(new_line)
     return screen
 
+def print_screen(screen):
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row])
+                     for row in screen]))
+
+
 
 screen_width = 6
 screen_height = 2
 
 screen = create_screen(screen_width, screen_height)
-print('\n'.join([''.join(['{:4}'.format(item) for item in row])
-      for row in screen]))
+print_screen(screen)
+
 
 
 # TODO: Create functions for each instruction type
